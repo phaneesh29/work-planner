18771f461bcbaddecccf4666fca26712f4c8c061
sreephaneesha05 <pes1ug23am314@pesu.pes.edu>
2025-11-10 22:54:31 +0530

    fix: correct apostrophe in "Don't have an account?" message

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -125,7 +125,7 @@ export default function LoginPage() {
 
           <div className="text-center">
             <p className="text-sm text-gray-400">
-              Don't have an account?{" "}
+              Don&apos;t have an account?{" "}
               <Link href="/register" className="font-medium text-blue-500 hover:text-blue-400">
                 Sign up
               </Link>
